7a8a55ddafcf570f702775753a37f1667992779e
yunisrasulzade <yunisrasulzada99@gmail.com>
2022-09-27 11:39:29 +0400

    gradle kts implemented

---
+++ buildSrc/src/main/java/ApplicationVersions.kt
@@ -0,0 +1,43 @@
+object ApplicationVersions {
+
+    //Application plugin versions
+    const val applicationPlugin = "7.2.1"
+    const val androidLibVersion = "7.2.1"
+    const val jetbrainsKotlin = "1.7.0"
+    const val kotlinJvm = "1.7.10"
+    const val safeargs = "2.5.2"
+
+    //AppLibs
+    const val coreKtx = "1.7.0"
+    const val navigation = "2.3.5"
+    const val appcompat = "1.5.1"
+    const val material = "1.6.1"
+    const val constraintLayout = "2.1.4"
+
+    //Hilt
+    const val hilt = "2.43.2"
+    const val hilt_compiler = "2.42"
+
+    // Test
+    const val jUnit = "4.13.2"
+    const val extJunit = "1.1.3"
+    const val espresso = "3.4.0"
+    const val mockitoCore = "3.5.13"
+    const val mockk = "1.12.7"
+    const val coroutinesTest = "1.6.4"
+    const val coreTesting = "2.1.0"
+
+    //Network
+    const val retrofit = "2.9.0"
+    const val okHttp = "5.0.0-alpha.3"
+
+    // Coroutines
+    const val coroutinesCore = "1.6.4"
+    const val coroutinesAndroid = "1.6.4"
+    const val lifecycleViewModel = "2.5.1"
+
+    const val coil = "2.2.0"
+
+}
+
+
